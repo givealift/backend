b83634ff42ed7c429a8996edc659f25a6cfb6b66
Daniel Stefanik <danielstefanik36@gmail.com>
2018-03-14 22:00:16 +0100

    init

---
+++ src/main/java/com/agh/sp1/givealift/GivealiftApplication.java
@@ -0,0 +1,12 @@
+package com.agh.sp1.givealift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivealiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivealiftApplication.class, args);
+	}
+}
