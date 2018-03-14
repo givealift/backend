20aa3c16430f47d759a09681d17e9b801d30a116
Daniel Stefanik <danielstefanik36@gmail.com>
2018-03-14 23:35:03 +0100

    server skeleton

--- src/main/java/com/agh/sp1/givealift/GiveALiftApplication.java
+++ src/main/java/com/agh/sp1/givealift/GiveALiftApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GivealiftApplication {
+public class GiveALiftApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GivealiftApplication.class, args);
+		SpringApplication.run(GiveALiftApplication.class, args);
 	}
 }
