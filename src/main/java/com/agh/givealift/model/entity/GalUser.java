94bbf73fc17e8ade68556240ac38ad3350ca8ac6
Daniel Stefanik <danielstefanik36@gmail.com>
2018-03-23 00:23:08 +0100

    empty link fix

--- src/main/java/com/agh/givealift/model/entity/GalUser.java
+++ src/main/java/com/agh/givealift/model/entity/GalUser.java
@@ -1,8 +1,21 @@
 package com.agh.givealift.model.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class GalUser {
-//    private Long
+    private Long galUserId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getGalUserId() {
+        return galUserId;
+    }
+
+    public void setGalUserId(Long galUserId) {
+        this.galUserId = galUserId;
+    }
 }
