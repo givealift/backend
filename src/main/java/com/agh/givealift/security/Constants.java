8bd77e25e4f932204e212ff6efe7e55b85011a64
patryk-zygmunt <patrykzygmuntzx@gmail.com.com>
2018-04-04 18:32:05 +0200

    Spring Security authenticate with jwt token sign up, and login with facebook

---
+++ src/main/java/com/agh/givealift/security/Constants.java
@@ -0,0 +1,9 @@
+package com.agh.givealift.security;
+
+public class Constants {
+
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5*60*60;
+    public static final String SIGNING_KEY = "give_a_lift";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+}
