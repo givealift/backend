4b4d8e6f250bf3775e824800e67733cdc0bbeebd
Patryk Zygmunt <patryk.zygmunt@comarch.pl>
2018-04-26 09:19:32 +0200

    git test

--- src/main/java/com/agh/givealift/controller/RouteController.java
+++ src/main/java/com/agh/givealift/controller/RouteController.java
@@ -14,7 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequestMapping("/route")
 public class RouteController {
     private final RouteService routeService;
