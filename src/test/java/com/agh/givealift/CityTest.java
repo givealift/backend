0ccd6a31d878c05b149a286f3eb7d4687b9709e0
patryk-zygmunt <patrykzygmuntzx@gmail.com.com>
2018-05-27 17:14:26 +0200

    test

--- src/test/java/com/agh/givealift/CityTest.java
+++ src/test/java/com/agh/givealift/CityTest.java
@@ -1,29 +1,37 @@
 package com.agh.givealift;
 
+import com.agh.givealift.service.CityService;
+import com.stefanik.cod.controller.COD;
+import com.stefanik.cod.controller.CODFactory;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
 //import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CityTest {
-    // private final COD cod = CODFactory.c().get();
-/*   Zeby sprawdzić czy na travisie routeTest przejdzie
+    private final COD cod = CODFactory.c().get();
+
     @Autowired
     CityService cityService;
 
-    @Before
+   /* @Before
     public void before() {
         cityService.generate(100);
-    }
+    }*/
 
     @Test
     public void citySearch() {
-        CODGlobal.setImmersionLevel(3);
+
+   /*  CODGlobal.setImmersionLevel(3);
         List<City> result = cityService.search("k", 2);
-        cod.c().off().i("SEARCH", result);
-        assertThat(result).hasSize(2);
-    }*/
+        cod.c().off().i("SEARCH", result);*/
+        assertTrue(true);
+    }
 
 }
